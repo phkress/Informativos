d8c05d5c2ea4ff97af2e85a6dcf297f61bd51f29
phkress <phkress@gmail.com>
2021-01-19 18:00:02 -0300

    Card Obs

---
+++ web/src/components/CardObs/index.js
@@ -0,0 +1,16 @@
+import Card  from 'react-bootstrap/Card'
+
+function CardText() {
+
+  return (
+        <Card>
+          <Card.Body>
+            <Card.Text>
+            Obs: Atendimentos dentro do prazo deverão ser reforçados com o cliente que ainda se encontra dentro do prazo estipulado e solicitar que o mesmo aguarde o contato técnico.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+  );
+}
+
+export default CardText;
