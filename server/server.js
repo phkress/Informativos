c8cbd0a764e8afd6f95861561adcfca159dc63e5
phkress <phkress@gmail.com>
2021-02-10 17:38:03 -0300

    docker files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./src/config/express');
-require('./src/config/database')('mongodb://localhost/ScriptCallcenter');
+require('./src/config/database')('mongodb://db/ScriptCallcenter');
 
 http.createServer(app)
 .listen(3001, function() {
